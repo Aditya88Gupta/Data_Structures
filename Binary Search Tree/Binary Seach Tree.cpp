6d11b9a34e0ed7b22533071dd04c99e9191e07d7
Aditya88Gupta <prince88aditya@gmail.com>
2021-01-23 20:31:46 +0530

    Update Binary Seach Tree.cpp

--- Binary Search Tree/Binary Seach Tree.cpp
+++ Binary Search Tree/Binary Seach Tree.cpp
@@ -1,3 +1,6 @@
+
+// Implementation of Binary Search Tree
+
 #include<iostream>
 using namespace std;
 
@@ -24,10 +27,15 @@ class BSearchTree{
     void PostTrav(Node *cur);
     void PreTrav(Node *cur);
     void InTrav(Node *cur);
+    Node* Root();
 };
 
-Node* BSearchTree::find(Node *cur,int key){
+Node* BSearchTree::Root(){
+    return root;
+}
 
+Node* BSearchTree::find(Node *cur,int key){                  
+                                        // O(Height(Tree))
     if (cur->key == key)               // Normal Binary Search
         return cur;   
     else if(cur->key < key){
@@ -60,7 +68,7 @@ Node* BSearchTree::RightAncestor(Node *cur){
        return RightAncestor(cur->Parent);
 }
 
-Node* BSearchTree::Next(Node *cur){
+Node* BSearchTree::Next(Node *cur){      // O(Height(Tree))
 
     if (cur->Right != NULL)             // If Right child exists, then go right then left until can't go further left 
        return LeftDescendant(cur->Right);
@@ -68,8 +76,8 @@ Node* BSearchTree::Next(Node *cur){
        return RightAncestor(cur);      // If Right child does not exists, then find first ancestor with a greater key
 }
 
-void BSearchTree::RangedSearch(int lower,int upper,Node *Root){
-    
+void BSearchTree::RangedSearch(int lower,int upper,Node *Root){   
+                                      // O(n)
     Node* cur = find(Root,lower);
     if(cur->key>lower)
       cout<<cur->key<<" ";
@@ -80,7 +88,7 @@ void BSearchTree::RangedSearch(int lower,int upper,Node *Root){
     }  
 }
 
-void BSearchTree::Insert(int val){
+void BSearchTree::Insert(int val){    // O(Height(Tree))
 
     Node *new_node = new Node();
     new_node->key = val;
@@ -99,10 +107,10 @@ void BSearchTree::Insert(int val){
     }
 }
 
-void BSearchTree::Delete(int key){
+void BSearchTree::Delete(int key){     // O(Height(Tree))
     Node* Temp = find(root,key);
     if (Temp->Right == NULL){         // Promote Left Child
-        if(Temp->Left != NULL)        // Check if it's a Leaf Node 
+        if(Temp->Left != NULL)       // Check if it's a Leaf Node 
             (Temp->Left)->Parent = Temp->Parent;
         if(Temp->key>(Temp->Parent)->key){
             (Temp->Parent)->Right = Temp->Left;
@@ -135,7 +143,7 @@ void BSearchTree::Delete(int key){
     }
 }
 
-void BSearchTree::PreTrav(Node *cur){
+void BSearchTree::PreTrav(Node *cur){   // O(n)
     
     if (cur==NULL)
        return;
@@ -145,7 +153,7 @@ void BSearchTree::PreTrav(Node *cur){
     PreTrav(cur->Right);
 }
 
-void BSearchTree::PostTrav(Node *cur){
+void BSearchTree::PostTrav(Node *cur){   // O(n)
     
     if (cur==NULL)
        return;
@@ -155,7 +163,7 @@ void BSearchTree::PostTrav(Node *cur){
     cout<<cur->key<<" ";
 }
 
-void BSearchTree::InTrav(Node *cur){
+void BSearchTree::InTrav(Node *cur){   // O(n)
     
     if (cur==NULL)
        return;
@@ -165,5 +173,61 @@ void BSearchTree::InTrav(Node *cur){
     InTrav(cur->Right);
 }
 
+
+int main(){
+    
+    BSearchTree tree;
+    int choice;int ele;
+    bool flag = true;
+    int x;
+    while(flag){
+
+        cout<<"1.)Insert"<<endl;
+        cout<<"2.)Delete"<<endl;
+        cout<<"3.)Infix Traversal"<<endl;
+        cout<<"4.)Prefix Traversal"<<endl;
+        cout<<"5.)Postfix Traversal"<<endl;
+        cout<<"6.)Range Search"<<endl;
+        cout<<"7.)Exit"<<endl;
+        cout<<"Enter Your Choice=";cin>>choice;
+        switch(choice){
+            case 1:
+                cout<<"Enter The Element To Be Inserted =";
+                cin>>ele;
+                tree.Insert(ele);
+                break;
+            case 2:
+                cout<<"Enter The Element To Be Deleted =";
+                cin>>x;
+                tree.Delete(x);
+                break;
+            case 3:
+                tree.InTrav(tree.Root());
+                cout<<endl;
+                break;
+            case 4:
+                tree.PreTrav(tree.Root());
+                cout<<endl;
+                break; 
+            case 5:
+                tree.PostTrav(tree.Root());
+                cout<<endl;
+                break;
+            case 6:
+                int l,u;
+                cout<<"Enter lower bound=";cin>>l;
+                cout<<"Enter upper bound=";cin>>u;
+                tree.RangedSearch(l,u,tree.Root());
+                cout<<endl;
+                break;    
+            case 7:
+                flag = false; 
+                break;   
+            default:
+                flag = false;
+                break;
+        }
+    }
+}
 
 
